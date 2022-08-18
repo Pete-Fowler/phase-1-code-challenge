64013a5a89090fb1d56379df2a054d6ff9d5f248
Pete Fowler <fowler.pete@gmail.com>
2022-08-18 10:22:30 -0600

    Display names

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+const bar = document.querySelector('#character-bar');
+
+const displayCharacters = (data) => {
+  data.forEach((char) => {
+    const span = document.createElement('span');
+    span.textContent = char.name;
+    bar.append(span);
+  });
+}
 
 const getCharacters = () => {
   fetch(`http://localhost:3000/characters`)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
-    // displayChars(data);
+    console.log(data)
+    displayCharacters(data);
   });
 }
 
