4810226e48934eaf7cd1ccc730f1ff681b3ef6ea
Pete Fowler <fowler.pete@gmail.com>
2022-08-18 10:17:32 -0600

    Add fetch request

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// Your code here
+
+const getCharacters = () => {
+  fetch(`http://localhost:3000/characters`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    // displayChars(data);
+  });
+}
+
+const init = (() => {
+  getCharacters();
+})();
