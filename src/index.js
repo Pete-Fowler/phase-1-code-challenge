6bdd76f466845f3e7be9962742c9646e310f6eed
Pete Fowler <fowler.pete@gmail.com>
2022-08-18 10:28:57 -0600

    Display details

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const bar = document.querySelector('#character-bar');
+const title = document.querySelector('#name');
+const image = document.querySelector('#image');
+
+const displayDetail = (character) => {
+  title.textContent = character.name;
+  image.src = character.image;
+}
 
 const displayCharacters = (data) => {
-  data.forEach((char) => {
+  data.forEach((character) => {
     const span = document.createElement('span');
-    span.textContent = char.name;
+    span.textContent = character.name;
+    span.addEventListener('click', () => displayDetail(character));
     bar.append(span);
   });
 }
@@ -12,7 +20,6 @@ const getCharacters = () => {
   fetch(`http://localhost:3000/characters`)
   .then(res => res.json())
   .then(data => {
-    console.log(data)
     displayCharacters(data);
   });
 }
